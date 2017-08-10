9956221e9728b1bf64ec007161d84159964c0a3f
Raphael Santos <raphael.santos@tse.jus.br>
2017-08-10 09:48:37 -0300

    Implementação do método de edição

--- src/main/java/br/com/alura/loja/servico/CarrinhoResource.java
+++ src/main/java/br/com/alura/loja/servico/CarrinhoResource.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -16,25 +17,28 @@ import com.thoughtworks.xstream.XStream;
 
 import br.com.alura.loja.dao.CarrinhoDAO;
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 
 @Path("carrinhos")
 public class CarrinhoResource {
 
 
 	/**
+	 * 
+	 * 
 	 * Uso recomendado, pois o cache será correto a uri
 	 * @param id
 	 * @return
 	 */
 	@Path("{id}")
-	@GET
+	@GET //O GET é um recurso usado para consulta informações
 	@Produces(MediaType.APPLICATION_XML)
 	public String busca(@PathParam("id") long id) {
 		Carrinho carrinho = new CarrinhoDAO().busca(id);
 		return carrinho.toXML();
 	}
 	
-	@POST
+	@POST //O POST é um recurso usado para inclusão de informações
 	@Consumes(MediaType.APPLICATION_XML)
 	public Response adiciona(String conteudo) {
 		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
@@ -45,10 +49,21 @@ public class CarrinhoResource {
 	
 	
 	@Path("{id}/produto/{produtoId}")
-	@DELETE
+	@DELETE //O DELETE é um recurso usado para deletar
 	public Response deletaProdutocarrinho(@PathParam("id") long id, @PathParam("produtoId") long produtoId) {
 		Carrinho carrinho = new CarrinhoDAO().busca(id);
 		carrinho.remove(produtoId);
+		return Response.ok().build();
+	}
+	
+	
+	@Path("{id}/produtos/{produtoId}/quantidade")
+	@PUT
+	@Consumes(MediaType.APPLICATION_XML)
+	public Response alterarProduto(String conteudo, @PathParam("id") long id, @PathParam("produtoId") long produtoId) {
+		Carrinho carrinho = new CarrinhoDAO().busca(id);
+		Produto produto = (Produto) new XStream().fromXML(conteudo);
+		carrinho.trocaQuantidade(produto);
 		return Response.ok().build();
 	}
 
